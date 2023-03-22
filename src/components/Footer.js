3b40a853d8aa2202ec2279fd37831ab1823d1015
PeppZZon <jespersimonlind@gmail.com>
2023-03-22 20:26:43 +0100

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <>
-      <div className="bg-gray-500 h-16 flex justify-center items-center bottom-0 fixed w-screen">
+      <div className="bg-gray-500 h-16 flex justify-center items-center bottom-0 w-screen">
         <p className="text-black font-body">
           &copy; 2023 Samfällighetsföreningen Stenbocken
         </p>
